9bec0d89e05626af506058db77f75e870aa378da
freshartclub <adminfreshart@gmail.com>
2024-09-24 12:09:26 +0530

    config file changed

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
   plugins: [
     react(),
     checker({
-      typescript: true,
+      typescript: false,
       eslint: {
         lintCommand: 'eslint "./src/**/*.{js,jsx,ts,tsx}"',
       },
